09ea079fa31395f157fc1a4f56e1dd60137b78f2
Amulya2406 <amulyanayakd24@gmail.com>
2023-07-21 14:23:32 +0530

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+
+import UserForm from './UserForm';
+
+test('renders learn react link', () => {
+  render(<UserForm/>);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
